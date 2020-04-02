3dfbe96bb41b7778c5c9a79dc6999c5b3002a0c8
Thada Light <thada1775@gmail.com>
2020-04-02 17:30:22 +0700

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    //[Route("[Controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Plus(int? id)
+        {
+            ViewData["plus"] = id + 10;
+            return View();
+        }
+        
+        public IActionResult Multiply(int? id)
+        {
+            ViewData["multiply"] = id * 10;
+            return View();
+        }
+        
+        public IActionResult Minus(int? num1, int? num2)
+        {
+            ViewData["minus"] = num2 - num1;
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactPost(Contact model)
+        {
+            ViewData["Name"] = model.Name;
+            ViewData["Email"] = model.Email;
+            ViewData["Subject"] = model.Subject;
+            ViewData["Message"] = model.Message;
+            return View();
+            //return Redirect("index");
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        
+        public string Message { get; set; }
+    }
+}
